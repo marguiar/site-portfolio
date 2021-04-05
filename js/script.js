92dcd9712df204a9757e5488eed8511e147e1fa1
Marcos Antonio <marcos-rj@outlook.com>
2021-04-04 21:46:05 -0300

    creating a spa in vanilla js

--- js/script.js
+++ js/script.js
@@ -31,3 +31,57 @@ function toggleTheme() {
 }
 
 toggleTheme();
+const links = document.querySelectorAll('ul a');
+
+function singlePageApplication() {
+  const links = document.querySelectorAll('ul a');
+  links[0].classList.add('active');
+
+  function updatePage(newContent) {
+    const container = document.createElement('div');
+    container.innerHTML = newContent;
+
+    const oldPage = document.querySelector('#root');
+    const newPage = container.querySelector('#root');
+    const oldTitle = document.querySelector('.title-container');
+    const newTitle = container.querySelector('.title-container');
+    oldPage.innerHTML = newPage.innerHTML;
+    oldTitle.innerHTML = newTitle.innerHTML;
+    document.title = container.querySelector('title').innerText;
+  }
+
+  async function fetchPage(url) {
+    const response = await fetch(url);
+    const pageContent = await response.text();
+    updatePage(pageContent);
+  }
+
+  function handleClick(e) {
+    e.preventDefault();
+    const url = e.target.href;
+
+    fetchPage(url);
+    window.history
+      .pushState(
+        null, null,
+        url.replace('pages/', '').replace('.html', '').replace('index', ''),
+      );
+  }
+
+  window.addEventListener('popstate', () => {
+    let previousPage = window.location.pathname;
+
+    if (previousPage === '/sobre') {
+      previousPage = ('/pages/sobre.html');
+    }
+
+    fetchPage(previousPage);
+  });
+
+  links.forEach((link) => {
+    link.classList.remove('active');
+    link.addEventListener('click', handleClick);
+  });
+}
+
+singlePageApplication();
